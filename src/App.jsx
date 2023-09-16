989203335c1f4c07e8bcf0cc013a941169548257
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-16 18:09:13 +0200

    llamada api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/results.json";
+import { searchMovies } from "./services/movies";
 
 function useSearch() {
   const [search, updateSearch] = useState("");
@@ -30,22 +31,37 @@ function useSearch() {
   return { search, updateSearch, error };
 }
 
+function useMovies({ search }) {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const previousSearch = useRef(search);
+
+  const getMovies = useCallback(async () => {
+    if (search === previousSearch.current) return;
+    try {
+      setLoading(true);
+      setError(null);
+      previousSearch.current = search;
+      const movies = await searchMovies({ search });
+      setMovies(movies);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [search]);
+
+  return { movies, loading, error, getMovies };
+}
+
 function App() {
   const { search, updateSearch, error } = useSearch();
-  const movies = responseMovies.Search;
-
-  const mappedMovies = movies.map((movie) => {
-    return {
-      id: movie.imdbID,
-      title: movie.Title,
-      year: movie.Year,
-      poster: movie.Poster,
-    };
-  });
+  const { movies, loading, getMovies } = useMovies({ search });
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Buscando", search);
+    getMovies({ search });
   };
 
   const handleChange = (event) => {
@@ -69,9 +85,7 @@ function App() {
         {error && <p className="error">{error}</p>}
       </header>
 
-      <main>
-        {/* {loading ? <p>Loading...</p> : <Movies movies={mappedMovies} />} */}
-      </main>
+      <main>{loading ? <p>Cargando...</p> : <Movies movies={movies} />}</main>
     </div>
   );
 }
--- src/services/movies.js
+++ src/services/movies.js
@@ -1,2 +1,22 @@
-const API_KEY_OMDB = "6c52a547";
-const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY_OMDB}&s=${search}`;
+export const searchMovies = async ({ search }) => {
+  const API_KEY_OMDB = "6c52a547";
+  const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY_OMDB}&s=${search}`;
+
+  if (search === "") return null;
+
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    const movies = data.Search;
+    return movies.map((movie) => {
+      return {
+        id: movie.imdbID,
+        title: movie.Title,
+        year: movie.Year,
+        poster: movie.Poster,
+      };
+    });
+  } catch (error) {
+    throw new Error("No se ha podido realizar la búsqueda");
+  }
+};
